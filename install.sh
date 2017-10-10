f7461ed8efd5430997d2f4a2df4f294d3da42b8f
Jonathan Adams <jadams89@lancers.lenoircc.edu>
2017-10-10 10:46:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ output "Make sure you double check before hitting enter! Only one shot at these!
 output ""
     read -e -p "Enter time zone (e.g. America/New_York) : " TIME
     read -e -p "Server name (no http:// or www. just example.com) : " server_name
+    read -e -p "Are you using a subdomain (pool.example.com?) [y/N] : " sub_domain
     read -e -p "Enter support email (e.g. admin@example.com) : " EMAIL
     read -e -p "Set stratum to AutoExchange? i.e. mine any coinf with BTC address? [y/N] : " BTC
     read -e -p "Please enter a new location for /site/adminRights this is to customize the admin entrance url (e.g. myAdminpanel) : " admin_panel
@@ -230,6 +231,204 @@ sudo chmod +x /var/stratum/config/run.sh
       sudo mkdir -p /var/www/$server_name/html  
     output "Creating webserver initial config file"
     output ""
+    if [[ ("$sub_domain" == "y" || "$sub_domain" == "Y") ]]; then
+echo 'include /etc/nginx/blockuseragents.rules;
+	server {
+	if ($blockedagent) {
+                return 403;
+        }
+        if ($request_method !~ ^(GET|HEAD|POST)$) {
+        return 444;
+        }
+        listen 80;
+        listen [::]:80;
+        server_name '"${server_name}"';
+        root "/var/www/'"${server_name}"'/html/web";
+        index index.html index.htm index.php;
+        charset utf-8;
+    
+        location / {
+        try_files $uri $uri/ /index.php?$args;
+        }
+        location @rewrite {
+        rewrite ^/(.*)$ /index.php?r=$1;
+        }
+    
+        location = /favicon.ico { access_log off; log_not_found off; }
+        location = /robots.txt  { access_log off; log_not_found off; }
+    
+        access_log off;
+        error_log  /var/log/nginx/'"${server_name}"'.app-error.log error;
+    
+        # allow larger file uploads and longer script runtimes
+ 	client_body_buffer_size  50k;
+        client_header_buffer_size 50k;
+        client_max_body_size 50k;
+        large_client_header_buffers 2 50k;
+        sendfile off;
+    
+        location ~ ^/index\.php$ {
+            fastcgi_split_path_info ^(.+\.php)(/.+)$;
+            fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+            fastcgi_index index.php;
+            include fastcgi_params;
+            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            fastcgi_intercept_errors off;
+            fastcgi_buffer_size 16k;
+            fastcgi_buffers 4 16k;
+            fastcgi_connect_timeout 300;
+            fastcgi_send_timeout 300;
+            fastcgi_read_timeout 300;
+	    try_files $uri $uri/ =404;
+        }
+		location ~ \.php$ {
+        	return 404;
+        }
+		location ~ \.sh {
+		return 404;
+        }
+		location ~ /\.ht {
+		deny all;
+        }
+		location ~ /.well-known {
+		allow all;
+        }
+		location /phpmyadmin {
+  		root /usr/share/;
+  		index index.php;
+  		try_files $uri $uri/ =404;
+  		location ~ ^/phpmyadmin/(doc|sql|setup)/ {
+    		deny all;
+  	}
+  		location ~ /phpmyadmin/(.+\.php)$ {
+    		fastcgi_pass unix:/run/php/php7.0-fpm.sock;
+    		fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    		include fastcgi_params;
+    		include snippets/fastcgi-php.conf;
+  	}
+ }
+ }
+' | sudo -E tee /etc/nginx/sites-available/$server_name.conf >/dev/null 2>&1
+
+sudo ln -s /etc/nginx/sites-available/$server_name.conf /etc/nginx/sites-enabled/$server_name.conf
+sudo ln -s /var/web /var/www/$server_name/html
+sudo service nginx restart
+	if [[ ("$ssl_install" == "y" || "$ssl_install" == "Y" || "$ssl_install" == "") ]]; then
+    output "Install LetsEncrypt and setting SSL"
+    sudo aptitude -y install letsencrypt
+    sudo letsencrypt certonly -a webroot --webroot-path=/var/web --email "$EMAIL" --agree-tos -d "$server_name"
+    sudo rm /etc/nginx/sites-available/$server_name.conf
+    sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+    # I am SSL Man!
+echo 'include /etc/nginx/blockuseragents.rules;
+	server {
+	if ($blockedagent) {
+                return 403;
+        }
+        if ($request_method !~ ^(GET|HEAD|POST)$) {
+        return 444;
+        }
+        listen 80;
+        listen [::]:80;
+        server_name '"${server_name}"';
+    	# enforce https
+        return 301 https://$server_name$request_uri;
+	}
+	
+	server {
+	if ($blockedagent) {
+                return 403;
+        }
+        if ($request_method !~ ^(GET|HEAD|POST)$) {
+        return 444;
+        }
+            listen 443 ssl http2;
+            listen [::]:443 ssl http2;
+            server_name '"${server_name}"';
+        
+            root /var/www/'"${server_name}"'/html/web;
+            index index.php;
+        
+            access_log /var/log/nginx/'"${server_name}"'.app-accress.log;
+            error_log  /var/log/nginx/'"${server_name}"'.app-error.log error;
+        
+            # allow larger file uploads and longer script runtimes
+ 	client_body_buffer_size  50k;
+        client_header_buffer_size 50k;
+        client_max_body_size 50k;
+        large_client_header_buffers 2 50k;
+        sendfile off;
+        
+            # strengthen ssl security
+            ssl_certificate /etc/letsencrypt/live/'"${server_name}"'/fullchain.pem;
+            ssl_certificate_key /etc/letsencrypt/live/'"${server_name}"'/privkey.pem;
+            ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+            ssl_prefer_server_ciphers on;
+            ssl_session_cache shared:SSL:10m;
+            ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:ECDHE-RSA-AES128-GCM-SHA256:AES256+EECDH:DHE-RSA-AES128-GCM-SHA256:AES256+EDH:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES128-SHA256:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256:AES256-SHA:AES128-SHA:DES-CBC3-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4";
+            ssl_dhparam /etc/ssl/certs/dhparam.pem;
+        
+            # Add headers to serve security related headers
+            add_header Strict-Transport-Security "max-age=15768000; preload;";
+            add_header X-Content-Type-Options nosniff;
+            add_header X-XSS-Protection "1; mode=block";
+            add_header X-Robots-Tag none;
+            add_header Content-Security-Policy "frame-ancestors 'self'";
+        
+        location / {
+        try_files $uri $uri/ /index.php?$args;
+        }
+        location @rewrite {
+        rewrite ^/(.*)$ /index.php?r=$1;
+        }
+    
+        
+            location ~ ^/index\.php$ {
+                fastcgi_split_path_info ^(.+\.php)(/.+)$;
+                fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+                fastcgi_index index.php;
+                include fastcgi_params;
+                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+                fastcgi_intercept_errors off;
+                fastcgi_buffer_size 16k;
+                fastcgi_buffers 4 16k;
+                fastcgi_connect_timeout 300;
+                fastcgi_send_timeout 300;
+                fastcgi_read_timeout 300;
+                include /etc/nginx/fastcgi_params;
+	    	try_files $uri $uri/ =404;
+        }
+		location ~ \.php$ {
+        	return 404;
+        }
+		location ~ \.sh {
+		return 404;
+        }
+        
+            location ~ /\.ht {
+                deny all;
+            }
+	    location /phpmyadmin {
+  		root /usr/share/;
+  		index index.php;
+  		try_files $uri $uri/ =404;
+  		location ~ ^/phpmyadmin/(doc|sql|setup)/ {
+    		deny all;
+  	}
+  		location ~ /phpmyadmin/(.+\.php)$ {
+    		fastcgi_pass unix:/run/php/php7.0-fpm.sock;
+    		fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    		include fastcgi_params;
+    		include snippets/fastcgi-php.conf;
+  	}
+ }
+ }
+        
+' | sudo -E tee /etc/nginx/sites-available/$server_name.conf >/dev/null 2>&1
+	fi
+sudo service nginx restart
+sudo service php7.0-fpm reload
+fi
 echo 'include /etc/nginx/blockuseragents.rules;
 	server {
 	if ($blockedagent) {
@@ -314,7 +513,7 @@ sudo service nginx restart
 	if [[ ("$ssl_install" == "y" || "$ssl_install" == "Y" || "$ssl_install" == "") ]]; then
     output "Install LetsEncrypt and setting SSL"
     sudo aptitude -y install letsencrypt
-    sudo letsencrypt certonly -a webroot --webroot-path=/var/web --email "$EMAIL" --agree-tos -d "$server_name" -d www."$server_name"
+    sudo letsencrypt certonly -a webroot --webroot-path=/var/web --email "$EMAIL" --agree-tos -d "$server_name" -d www.'"${server_name}"'
     sudo rm /etc/nginx/sites-available/$server_name.conf
     sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
     # I am SSL Man!
@@ -426,6 +625,7 @@ echo 'include /etc/nginx/blockuseragents.rules;
 	fi
 sudo service nginx restart
 sudo service php7.0-fpm reload
+
     clear
     output "Now for the database fun!"
     # create database
