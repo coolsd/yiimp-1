1b4b75e098163868f5378eebac564c4b29773598
xavatar94 <xavatar@wanadoo.fr>
2018-01-07 21:16:17 +0100

    add stratum algo

--- screen.sh
+++ screen.sh
@@ -7,3 +7,15 @@
  screen -dmS loop2 bash $WEB_DIR/loop2.sh
  screen -dmS blocks bash $WEB_DIR/blocks.sh
  screen -dmS debug tail -f $LOG_DIR/debug.log
+ 
+ screen -dmS groestl $STRATUM_DIR/run.sh groestl
+ screen -dmS keccak $STRATUM_DIR/run.sh keccak
+ screen -dmS neoscrypt $STRATUM_DIR/run.sh neoscrypt
+ screen -dmS nist5 $STRATUM_DIR/run.sh nist5
+ screen -dmS quark $STRATUM_DIR/run.sh quark
+ screen -dmS scrypt $STRATUM_DIR/run.sh scrypt
+ screen -dmS skein $STRATUM_DIR/run.sh skein
+ screen -dmS x11 $STRATUM_DIR/run.sh x11
+ screen -dmS xevan $STRATUM_DIR/run.sh xevan
+ 
+ 
