14555c6a40d1f77fc338167479928a87f85ad7a1
xavatar94 <xavatar@wanadoo.fr>
2018-01-08 14:45:42 +0100

    modify neo

--- screen.sh
+++ screen.sh
@@ -10,7 +10,7 @@
  
  screen -dmS groestl $STRATUM_DIR/run.sh groestl
  screen -dmS keccak $STRATUM_DIR/run.sh keccak
- screen -dmS neoscrypt $STRATUM_DIR/run.sh neoscrypt
+ screen -dmS neoscrypt $STRATUM_DIR/run.sh neo
  screen -dmS nist5 $STRATUM_DIR/run.sh nist5
  screen -dmS quark $STRATUM_DIR/run.sh quark
  screen -dmS scrypt $STRATUM_DIR/run.sh scrypt
