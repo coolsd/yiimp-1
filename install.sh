78ce689736def326365326b3f78da893ffeb4d04
manfromafar <jonsonb10@gmail.com>
2018-02-11 11:06:33 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ clear
 output "Make sure you double check before hitting enter! Only one shot at these!"
 output ""
     read -e -p "Enter time zone (e.g. America/New_York) : " TIME
-    read -e -p "Server name (no http:// or www. just example.com) : " server_name
+#    read -e -p "Server name (no http:// or www. just example.com) : " server_name
     read -e -p "Are you using a subdomain (pool.example.com?) [y/N] : " sub_domain
     read -e -p "Enter support email (e.g. admin@example.com) : " EMAIL
     read -e -p "Set stratum to AutoExchange? i.e. mine any coinf with BTC address? [y/N] : " BTC
