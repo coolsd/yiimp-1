b40f29d76a81ae6f516b2cd454f7cf0b046daf83
xavatar <xavatar@wanadoo.fr>
2018-02-14 00:26:09 +0100

    fix typo

--- install.sh
+++ install.sh
@@ -116,7 +116,7 @@ default         0;
     output "Installing Package to compile crypto currency"
     output " "
     sudo aptitude -y install software-properties-common build-essential
-    sudo aptitude -y install  libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev zlib1g-dev libz-dev libseccomp-dev libcap-dev libminiupnpc-dev
+    sudo aptitude -y install libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev zlib1g-dev libz-dev libseccomp-dev libcap-dev libminiupnpc-dev
     sudo aptitude -y install libminiupnpc10 libzmq5
     sudo aptitude -y install libcanberra-gtk-module libqrencode-dev libzmq3-dev
     sudo aptitude -y install libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
