be8aec217fc32525b8ac08c564a96bbc34fcbfbf
Tatar Xavatar <xavatar@wanadoo.fr>
2018-06-26 11:35:20 +0200

    fix typo

--- install.sh
+++ install.sh
@@ -114,7 +114,7 @@ default         0;
     sudo aptitude -y install git screen
     sudo aptitude -y install pwgen -y
 	
-	sudo systemctl start php7.0-fpm.service
+    sudo systemctl start php7.0-fpm.service
 	
     #Installing Package to compile crypto currency
     output " "
@@ -143,8 +143,8 @@ default         0;
     sleep 3
     
     if [[ "$root_email" != "" ]]; then
-    echo $root_email > sudo tee --append ~/.email
-    echo $root_email > sudo tee --append ~/.forward
+        echo $root_email > sudo tee --append ~/.email
+        echo $root_email > sudo tee --append ~/.forward
 
     if [[ ("$send_email" == "y" || "$send_email" == "Y" || "$send_email" == "") ]]; then
         echo "This is a mail test for the SMTP Service." > sudo tee --append /tmp/email.message
@@ -174,60 +174,60 @@ default         0;
     sudo ufw allow ssh
     sudo ufw allow http
     sudo ufw allow https
-	sudo ufw allow 3333/tcp
-	sudo ufw allow 3339/tcp
-	sudo ufw allow 3334/tcp
-	sudo ufw allow 3433/tcp
-	sudo ufw allow 3555/tcp
-	sudo ufw allow 3556/tcp
-	sudo ufw allow 3573/tcp
-	sudo ufw allow 3535/tcp
-	sudo ufw allow 3533/tcp
-	sudo ufw allow 3553/tcp
-	sudo ufw allow 3633/tcp
-	sudo ufw allow 3733/tcp
-	sudo ufw allow 3636/tcp
-	sudo ufw allow 3737/tcp
-	sudo ufw allow 3739/tcp
-	sudo ufw allow 3747/tcp
-	sudo ufw allow 3833/tcp
-	sudo ufw allow 3933/tcp
-	sudo ufw allow 4033/tcp
-	sudo ufw allow 4133/tcp
-	sudo ufw allow 4233/tcp
-	sudo ufw allow 4234/tcp
-	sudo ufw allow 4333/tcp
-	sudo ufw allow 4433/tcp
-	sudo ufw allow 4533/tcp
-	sudo ufw allow 4553/tcp
-	sudo ufw allow 4633/tcp
-	sudo ufw allow 4733/tcp
-	sudo ufw allow 4833/tcp
-	sudo ufw allow 4933/tcp
-	sudo ufw allow 5033/tcp
-	sudo ufw allow 5133/tcp
-	sudo ufw allow 5233/tcp
-	sudo ufw allow 5333/tcp
-	sudo ufw allow 5433/tcp
-	sudo ufw allow 5533/tcp
-	sudo ufw allow 5733/tcp
-	sudo ufw allow 5743/tcp
-	sudo ufw allow 3252/tcp
-	sudo ufw allow 5755/tcp
-	sudo ufw allow 5766/tcp
-	sudo ufw allow 5833/tcp
-	sudo ufw allow 5933/tcp
-	sudo ufw allow 6033/tcp
-	sudo ufw allow 5034/tcp
-	sudo ufw allow 6133/tcp
-	sudo ufw allow 6233/tcp
-	sudo ufw allow 6333/tcp
-	sudo ufw allow 6433/tcp
-	sudo ufw allow 7433/tcp
-	sudo ufw allow 8333/tcp
-	sudo ufw allow 8463/tcp
-	sudo ufw allow 8433/tcp
-	sudo ufw allow 8533/tcp
+    sudo ufw allow 3333/tcp
+    sudo ufw allow 3339/tcp
+    sudo ufw allow 3334/tcp
+    sudo ufw allow 3433/tcp
+    sudo ufw allow 3555/tcp
+    sudo ufw allow 3556/tcp
+    sudo ufw allow 3573/tcp
+    sudo ufw allow 3535/tcp
+    sudo ufw allow 3533/tcp
+    sudo ufw allow 3553/tcp
+    sudo ufw allow 3633/tcp
+    sudo ufw allow 3733/tcp
+    sudo ufw allow 3636/tcp
+    sudo ufw allow 3737/tcp
+    sudo ufw allow 3739/tcp
+    sudo ufw allow 3747/tcp
+    sudo ufw allow 3833/tcp
+    sudo ufw allow 3933/tcp
+    sudo ufw allow 4033/tcp
+    sudo ufw allow 4133/tcp
+    sudo ufw allow 4233/tcp
+    sudo ufw allow 4234/tcp
+    sudo ufw allow 4333/tcp
+    sudo ufw allow 4433/tcp
+    sudo ufw allow 4533/tcp
+    sudo ufw allow 4553/tcp
+    sudo ufw allow 4633/tcp
+    sudo ufw allow 4733/tcp
+    sudo ufw allow 4833/tcp
+    sudo ufw allow 4933/tcp
+    sudo ufw allow 5033/tcp
+    sudo ufw allow 5133/tcp
+    sudo ufw allow 5233/tcp
+    sudo ufw allow 5333/tcp
+    sudo ufw allow 5433/tcp
+    sudo ufw allow 5533/tcp
+    sudo ufw allow 5733/tcp
+    sudo ufw allow 5743/tcp
+    sudo ufw allow 3252/tcp
+    sudo ufw allow 5755/tcp
+    sudo ufw allow 5766/tcp
+    sudo ufw allow 5833/tcp
+    sudo ufw allow 5933/tcp
+    sudo ufw allow 6033/tcp
+    sudo ufw allow 5034/tcp
+    sudo ufw allow 6133/tcp
+    sudo ufw allow 6233/tcp
+    sudo ufw allow 6333/tcp
+    sudo ufw allow 6433/tcp
+    sudo ufw allow 7433/tcp
+    sudo ufw allow 8333/tcp
+    sudo ufw allow 8463/tcp
+    sudo ufw allow 8433/tcp
+    sudo ufw allow 8533/tcp
     sudo ufw --force enable    
     fi
     
