66ec6d71c9fbffd58ab8abfdd18e79f92855e5b5
Xavatar <xavatar@wanadoo.fr>
2018-06-26 20:26:54 +0200

    add info server name

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ displayErr() {
     output "Make sure you double check before hitting enter! Only one shot at these!"
     output " "
     read -e -p "Enter time zone (e.g. America/New_York) : " TIME
-    read -e -p "Server name (no http:// or www. just example.com) : " server_name
+    read -e -p "Server name (no http:// or www. just : example.com or pool.example.com) : " server_name
     read -e -p "Are you using a subdomain (pool.example.com?) [y/N] : " sub_domain
     read -e -p "Enter support email (e.g. admin@example.com) : " EMAIL
     read -e -p "Set Pool to AutoExchange? i.e. mine any coin with BTC address? [y/N] : " BTC
