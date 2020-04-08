dbb143a8204b0c93086ef79bdf20c98278be5434
Tatar Xavatar <perrinx@outlook.fr>
2020-04-08 04:18:48 +0200

    add function.sh

--- test/conf/function.sh
+++ test/conf/function.sh
@@ -14,6 +14,22 @@ BLUE=$ESC_SEQ"34;01m"
 MAGENTA=$ESC_SEQ"35;01m"
 CYAN=$ESC_SEQ"36;01m"
 
+function spinner
+ {
+ 		local pid=$!
+ 		local delay=0.75
+ 		local spinstr='|/-\'
+ 		while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+ 				local temp=${spinstr#?}
+ 				printf " [%c]  " "$spinstr"
+ 				local spinstr=$temp${spinstr%"$temp"}
+ 				sleep $delay
+ 				printf "\b\b\b\b\b\b"
+ 		done
+ 		printf "    \b\b\b\b"
+ }
+
+
 function hide_output {
 		OUTPUT=$(tempfile)
 		$@ &> $OUTPUT & spinner
