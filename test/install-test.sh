9452fdfb8ee42d04661ae7b4f4077aba125b98de
Tatar Xavatar <perrinx@outlook.fr>
2020-04-10 01:23:13 +0200

    remove php-auth ubun18

--- test/install-test.sh
+++ test/install-test.sh
@@ -170,7 +170,7 @@
     #hide_output sudo phpenmod mbstring
     else
     apt_install php7.3-fpm php7.3-opcache php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-imap php7.3-cli \
-    php7.3-cgi php-pear php-auth imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt\
+    php7.3-cgi php-pear imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt\
     php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache php-imagick php-gettext php7.3-zip php7.3-mbstring \
     libpsl-dev libnghttp2-dev
     fi
