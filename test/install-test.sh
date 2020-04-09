4e7553a92af53ec256f419cfd074671dfe9e24e0
Tatar Xavatar <perrinx@outlook.fr>
2020-04-09 10:23:46 +0200

    remove php7.1-mcrypt

--- test/install-test.sh
+++ test/install-test.sh
@@ -161,10 +161,8 @@
     fi
     hide_output sudo apt -y update
     apt_install php7.3-fpm php7.3-opcache php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-imap php7.3-cli \
-    php7.3-cgi php-pear php-auth imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell \
+    php7.3-cgi php-pear php-auth imagemagick libruby php7.3-curl php7.3-intl php7.3-pspell mcrypt\
     php7.3-recode php7.3-sqlite3 php7.3-tidy php7.3-xmlrpc php7.3-xsl memcached php-memcache php-imagick php-gettext php7.3-zip php7.3-mbstring
-    apt_install php7.1-mcrypt
-    hide_output sudo ln -s /etc/php/7.1/mods-available/mcrypt.ini /etc/php/7.3/mods-available/
     hide_output sudo phpenmod mcrypt
     hide_output sudo sudo phpenmod mbstring
     sleep 5
